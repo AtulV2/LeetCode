2d875d50f1d760186731c3ce4e2fe39551d0b51b
AtulV2 <153658280+AtulV2@users.noreply.github.com>
2026-08-18 18:41:49 +0530

    Time: 7 ms (12.79%), Space: 12.5 MB (71.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def isValid(self, s):
+        
+        stack = []
+        for symbol in s:
+            if symbol in "([{":
+                stack.append(symbol)
+            
+            # if len(stack) != 0:
+            #     TopE = stack[-1]
+
+            #     if symbol == ")" and TopE == "(":
+            #         stack.pop()
+            #     if symbol == "]" and TopE == "[":
+            #         stack.pop()
+            #     if symbol == "}" and TopE == "{":
+            #         stack.pop()
+
+        
+            if symbol == ")":
+                stack.append(symbol)
+                if len(stack) >= 2 and stack[-2] == "(":
+                    stack.pop()
+                    stack.pop()
+            if symbol == "]":
+                stack.append(symbol)
+                if len(stack) >= 2 and stack[-2] == "[":
+                    stack.pop()
+                    stack.pop()
+            if symbol == "}":
+                stack.append(symbol)
+                if len(stack) >= 2 and stack[-2] == "{":
+                    stack.pop()
+                    stack.pop()
+
+
+        if len(stack) != 0:
+            return False
+        else:
+            return True
+
+
+                
+    
+
+        

6a0d928b9b4f933d43382fc6fbc8eb5bdedb3ee6
AtulV2 <153658280+AtulV2@users.noreply.github.com>
2026-09-01 10:40:03 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.57%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        numRows = rowIndex + 1
+        stack = []
+        for k in range(1,numRows+1):
+            if k == 1:
+                stack.append([1])
+            elif k == 2:
+                stack.append([1,1])
+            else:
+
+                preArr =  stack[-1]
+
+                temp = [1]
+                for i in range(0,len(preArr)-1):
+                    sum =  preArr[i] + preArr[i+1]
+                    temp.append(sum)
+                temp.append(1)
+
+                stack.append(temp)
+            
+        return stack[-1]
+
+
+
+        
